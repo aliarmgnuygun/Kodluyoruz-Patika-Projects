32f4f01844b26ddd7c9022fd0acd58f7e4a87a0d
Ali Armağan Uygun <75046911+aliarmgnuygun@users.noreply.github.com>
2022-06-28 15:08:16 +0300

    KDV Calculation Program

---
+++ KdvCalculate.java
@@ -0,0 +1,32 @@
+package CalculateKDV;
+
+import java.util.Scanner;
+
+public class KdvCalculate {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the KDV Calculation Program");
+        System.out.print("Please enter price: ");
+        int price = input.nextInt();
+
+        float kdv1 = 1.18f, kdv2 = 1.08f, newPrice = 0;
+
+        if (price > 1000){
+            newPrice = price*kdv2;
+            System.out.println("Price without KDV: " + price +
+                    "\nPrice with KDV: " + newPrice +
+                    "\nKDV amount: " + kdv2);
+
+
+        }else if( 0 < price && price <= 1000){
+            newPrice = price*kdv1;
+            System.out.println("Price without KDV: " + price +
+                    "\nPrice with KDV: " + newPrice +
+                    "\nKDV amount: " + kdv1);
+
+        }else
+            System.out.println("Invalid value! Please enter a value greater than 0.");
+
+    }
+}
