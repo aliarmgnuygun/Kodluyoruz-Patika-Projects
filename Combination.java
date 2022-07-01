ad73dfe82317de43d5cdd7a60925753f2dde5e17
Ali Armağan Uygun <75046911+aliarmgnuygun@users.noreply.github.com>
2022-07-01 15:19:21 +0300

    Update Combination.java

--- Combination.java
+++ Combination.java
@@ -4,28 +4,34 @@ import java.util.Scanner;
 
 public class Combination {
     public static void main(String[] args) {
+        // A program that calculates combinations is written.
+
         Scanner input = new Scanner(System.in);
         int n, r, c, nFac = 1, rFac = 1, nRFac = 1;
 
-            System.out.print("Please enter \"n\": ");
-            n = input.nextInt();
-            for (int i = 1; i <= n; i++) {
-                nFac *= i;
-            }
+        //Factorial for n
+        System.out.print("Please enter \"n\": ");
+        n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            nFac *= i;
+        }
 
-            System.out.print("Please enter \"r\": ");
-            r = input.nextInt();
-            for (int i = 1; i <= r; i++) {
-                rFac *= i;
-            }
+        //Factorial for r
+        System.out.print("Please enter \"r\": ");
+        r = input.nextInt();
+        for (int i = 1; i <= r; i++) {
+            rFac *= i;
+        }
 
-            for (int i = 1; i <= n - r; i++) {
-                nRFac *= i;
-            }
+        //Factorial for n-r
+        for (int i = 1; i <= n - r; i++) {
+            nRFac *= i;
+        }
 
-            c = (nFac) / (rFac * nRFac);
+        //formula
+        c = (nFac) / (rFac * nRFac);
 
-            System.out.println("Combination C(" + n + "," + r + "): " + c);
+        System.out.println("Combination C(" + n + "," + r + "): " + c);
     }
 }
 
